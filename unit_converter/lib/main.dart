2dca669bd6a4753d26e3e8f968b54dfa96b1196f
MuhamadHaydar <49116664+muhamadhaydar@users.noreply.github.com>
2020-06-15 17:52:00 +0300

    Init Measures Converter project.

---
+++ unit_converter/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Measures Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Measures Converter'),
+        ),
+        body: Center(
+          child: Text('Measures Converter'),
+        ),),
+    );}
+}
