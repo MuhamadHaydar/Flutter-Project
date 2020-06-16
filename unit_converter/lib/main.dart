7b0b27eab4dfa9ef2fcf131bbee3eaa8d1ff5f55
MuhamadHaydar <49116664+muhamadhaydar@users.noreply.github.com>
2020-06-16 10:37:25 +0300

    Add DropDownButton and fill it with a measures list of strings.

--- unit_converter/lib/main.dart
+++ unit_converter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -7,12 +8,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   double _numberForm;
+
+  final List<String> _measures = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilograms',
+    'feet',
+    'miles',
+    'pounds(lbs)',
+    'ounces'
+  ];
+
+  String _startMeasure;
 
   @override
   void initState() {
     _numberForm = 0;
+    _startMeasure = _measures[0];
     super.initState();
   }
 
@@ -28,11 +42,25 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              DropdownButton(
+                value: _startMeasure,
+                onChanged: (value) {
+                  setState(() {
+                    _startMeasure = value;
+                  });
+                },
+                items: _measures.map((String values) {
+                  return DropdownMenuItem<String>(
+                    child: Text(values),
+                    value: values,
+                  );
+                }).toList(),
+              ),
               Text(_numberForm.toString()),
               TextField(
-                onChanged: (text){
+                onChanged: (text) {
                   var inputNumber = double.parse(text);
-                  if(inputNumber != null){
+                  if (inputNumber != null) {
                     setState(() {
                       _numberForm = inputNumber;
                     });
@@ -41,6 +69,8 @@ class _MyAppState extends State<MyApp> {
               ),
             ],
           ),
-        ),),
-    );}
+        ),
+      ),
+    );
+  }
 }
