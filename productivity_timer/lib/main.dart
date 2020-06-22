e788eda8a5d1adbd135ae88b764f3a02e979056f
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-06-22 16:48:32 +0300

    Add CircularPercentIndicator package and put it in the middle Text place.

--- productivity_timer/lib/main.dart
+++ productivity_timer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:productivitytimer/widgets.dart';
 
 void main() => runApp(MyApp());
@@ -22,74 +23,90 @@ class TimerHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My work timer'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-              Expanded(
-                  child: ProductivityButton(
-                color: Color(0xff009688),
-                text: "Work",
-                onPressed: emptyMethod,
-              )),
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-              Expanded(
-                  child: ProductivityButton(
-                color: Color(0xff607D8B),
-                text: "Short Break",
-                onPressed: emptyMethod,
-              )),
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-              Expanded(
-                child: ProductivityButton(
-                  color: Color(0xff455A64),
-                  text: "Long Break",
-                  onPressed: emptyMethod,
+        appBar: AppBar(
+          title: Text('My work timer'),
+        ),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraint) {
+            // Get the width of the screen.
+            final double availableWidth = constraint.maxWidth;
+            return Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                        child: ProductivityButton(
+                      color: Color(0xff009688),
+                      text: "Work",
+                      onPressed: emptyMethod,
+                    )),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                        child: ProductivityButton(
+                      color: Color(0xff607D8B),
+                      text: "Short Break",
+                      onPressed: emptyMethod,
+                    )),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                      child: ProductivityButton(
+                        color: Color(0xff455A64),
+                        text: "Long Break",
+                        onPressed: emptyMethod,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                  ],
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-            ],
-          ),
-          Expanded(child: Text("Hello")),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-              Expanded(
-                  child: ProductivityButton(
-                      color: Color(0xff212121),
-                      text: 'Stop',
-                      onPressed: emptyMethod)),
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-              Expanded(
-                  child: ProductivityButton(
-                color: Color(0xff009688),
-                text: 'Restart',
-                onPressed: emptyMethod,
-              )),
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+                Expanded(
+                  child: CircularPercentIndicator(
+                    radius: availableWidth / 2,
+                    lineWidth: 10.0,
+                    percent: 1,
+                    center: Text(
+                      "30:00",
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                    progressColor: Color(0xff009688),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                        child: ProductivityButton(
+                            color: Color(0xff212121),
+                            text: 'Stop',
+                            onPressed: emptyMethod)),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                    Expanded(
+                        child: ProductivityButton(
+                      color: Color(0xff009688),
+                      text: 'Restart',
+                      onPressed: emptyMethod,
+                    )),
+                    Padding(
+                      padding: EdgeInsets.all(defaultPadding),
+                    ),
+                  ],
+                )
+              ],
+            );
+          },
+        ));
   }
 
   void emptyMethod() {}
