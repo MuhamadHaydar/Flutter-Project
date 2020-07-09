eb142930a7c8846ba8ff630b4c51be6825d2a22b
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-07-09 03:07:26 +0300

    Add SharedPreference package and prepare settings activity to work with shared preference and control text fields.

--- productivity_timer/lib/settings.dart
+++ productivity_timer/lib/settings.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:productivitytimer/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -20,6 +22,30 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   TextStyle textStyle = TextStyle(fontSize: 24);
 
+  // Used to control over the text fields.
+  TextEditingController txtWork;
+  TextEditingController txtShort;
+  TextEditingController txtLong;
+
+  static const String WORKTIME = "workTime";
+  static const String SHORTBREAK = "shortBreak";
+  static const String LONGBREAK = "longBreak";
+
+  int workTime;
+  int shortBreak;
+  int longBreak;
+
+  // Use shared preference to save data.
+  SharedPreferences prefs;
+
+  @override
+  void initState() {
+    txtWork = TextEditingController();
+    txtShort = TextEditingController();
+    txtLong = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -30,55 +56,60 @@ class _SettingsState extends State<Settings> {
       crossAxisSpacing: 10,
       mainAxisSpacing: 10,
       children: <Widget>[
-        Text("Work", style: textStyle),
-        Text(""),
-        Text(""),
-        SettingButton(Color(0xff455A64), "-", -1),
-        TextField(
-            style: textStyle,
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number),
-        SettingButton(
-          Color(0xff009688),
-          "+",
-          1,
-        ),
-        Text("Short", style: textStyle),
-        Text(""),
-        Text(""),
-        SettingButton(
-          Color(0xff455A64),
-          "-",
-          -1,
-        ),
-        TextField(
-            style: textStyle,
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number),
-        SettingButton(Color(0xff009688), "+", 1),
-        Text(
-          "Long",
-          style: textStyle,
-        ),
-        Text(""),
-        Text(""),
-        SettingButton(
-          Color(0xff455A64),
-          "-",
-          -1,
-        ),
-        TextField(
-          style: textStyle,
-          textAlign: TextAlign.center,
-          keyboardType: TextInputType.number,
-        ),
-        SettingButton(
-          Color(0xff009688),
-          "+",
-          1,
-        ),
-      ],
-      padding: const EdgeInsets.all(20.0),
-    ));
+            Text("Work", style: textStyle),
+            Text(""),
+            Text(""),
+            SettingButton(Color(0xff455A64), "-", -1),
+            TextField(
+              style: textStyle,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              controller: txtWork,
+            ),
+            SettingButton(
+              Color(0xff009688),
+              "+",
+              1,
+            ),
+            Text("Short", style: textStyle),
+            Text(""),
+            Text(""),
+            SettingButton(
+              Color(0xff455A64),
+              "-",
+              -1,
+            ),
+            TextField(
+              style: textStyle,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              controller: txtShort,
+            ),
+            SettingButton(Color(0xff009688), "+", 1),
+            Text(
+              "Long",
+              style: textStyle,
+            ),
+            Text(""),
+            Text(""),
+            SettingButton(
+              Color(0xff455A64),
+              "-",
+              -1,
+            ),
+            TextField(
+              style: textStyle,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              controller: txtLong,
+            ),
+            SettingButton(
+              Color(0xff009688),
+              "+",
+              1,
+            ),
+          ],
+          padding: const EdgeInsets.all(20.0),
+        ));
   }
 }
